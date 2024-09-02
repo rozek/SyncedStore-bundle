fdcb287e8ac56651d225a983625a6a33e76f61e4
Andreas Rozek <A.Rozek@gmx.de>
2024-09-02 11:43:44 +0200

    initial build

---
+++ dist/SyncedStore-bundle.d.ts
@@ -0,0 +1,9 @@
+export * from 'yjs';
+import { syncedStore, SyncedDoc, SyncedArray, SyncedMap, SyncedXml, SyncedText, Box, boxed, observeDeep, areSame, getYjsDoc, getYjsValue } from "@syncedstore/core";
+import { PREFERRED_TRIM_SIZE, fetchUpdates, storeState, clearDocument, IndexeddbPersistence } from 'y-indexeddb';
+import { WebsocketProvider, messageAuth, messageAwareness, messageQueryAwareness, messageSync } from 'y-websocket';
+import { WebrtcConn, Room, SignalingConn, WebrtcProvider } from 'y-webrtc';
+import { YKeyValue } from 'y-utility/y-keyvalue';
+import { LWWMap } from 'y-lwwmap';
+import { Observable } from 'lib0/observable';
+export { PREFERRED_TRIM_SIZE, fetchUpdates, storeState, clearDocument, IndexeddbPersistence, WebsocketProvider, messageAuth, messageAwareness, messageQueryAwareness, messageSync, WebrtcConn, Room, SignalingConn, WebrtcProvider, YKeyValue, LWWMap, Observable, syncedStore, SyncedDoc, SyncedArray, SyncedMap, SyncedXml, SyncedText, Box, boxed, observeDeep, areSame, getYjsDoc, getYjsValue };
